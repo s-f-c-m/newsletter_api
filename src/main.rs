50fb0795bf025ffd4e9cb7b442d105705185fcc4
Sebastian C <sebastian9111@gmail.com>
2022-07-19 15:37:18 +0300

    Add Secrecy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use newsletter_api::configuration::get_configuration;
 use newsletter_api::startup::run;
 use newsletter_api::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -10,9 +11,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     // env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
